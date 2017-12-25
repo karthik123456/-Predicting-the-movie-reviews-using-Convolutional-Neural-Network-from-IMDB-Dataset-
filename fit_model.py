278baa5c7d5f08da2e4ea79b1baf48348ad94c33
Karthik Hegde <kartikhegde12@gmail.com>
2017-12-25 15:07:29 +0530

    Delete fit_model.py

--- fit_model.py
+++
@@ -1,5 +0,0 @@
-# Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_size=128, verbose=2)
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
